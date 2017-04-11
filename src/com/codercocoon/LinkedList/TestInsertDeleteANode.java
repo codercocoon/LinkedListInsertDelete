379b8f27e8946a8c2652aa68a545dd2ebf9b1536
codercocoon <mounir.sahrani@gmail.com>
2017-04-11 18:32:27 +0100

    This example presents the basic concept of insert/delete a node of a linked list.
    This example presents the basic concept of insert/delete a node of a
    linked list.

--- src/com/codercocoon/LinkedList/TestInsertDeleteANode.java
+++ src/com/codercocoon/LinkedList/TestInsertDeleteANode.java
@@ -1,4 +1,4 @@
-package com.codercocoon.DoublyLinkedList;
+package com.codercocoon.LinkedList;
 
 import java.util.Scanner;
 
@@ -9,29 +9,28 @@ public class TestInsertDeleteANode {
 		Node head = null;
 		InsertDeleteANode ll = new InsertDeleteANode();
 
-		System.out.print("Enter the doubly linked list size : ");
+		System.out.print("Enter the linked list size : ");
 		int size = sc.nextInt();
 
-		System.out.println("##### Insert at last of the doubly linked list. #####");
+		System.out.println("##### Insert at last of the linked list. #####");
 		int tmp = size;
-		System.out.println("Enter the doubly linked list elements : ");
+		System.out.println("Enter the linked list elements : ");
 		while (tmp > 0) {
 			head = ll.insertAtLast(head, sc.nextInt());
 			tmp--;
 		}
 		ll.print(head);
 		
-		System.out.println("##### Insert at first of the doubly linked list. #####");
+		System.out.println("##### Insert at first of the linked list. #####");
 		System.out.println("Enter the element to insert at first : ");
 		head = ll.insertAtFirst(head, sc.nextInt());
-		
-		System.out.println("##### Insert at Nth position of the doubly linked list. #####");
+
+		System.out.println("##### Insert at Nth position of the linked list. #####");
 		System.out.println("Enter the element to insert and the position : ");
 		head = ll.InsertNth(head, sc.nextInt(), sc.nextInt());
 
-		System.out.println("##### Delete element from the doubly linked list. #####");
+		System.out.println("##### Delete element from the linked list. #####");
 		System.out.println("Enter the position to delete : ");
 		head = ll.delete(head, sc.nextInt());
-		
 	}
 }
