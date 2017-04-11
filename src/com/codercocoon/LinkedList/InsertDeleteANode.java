3fd93358782f620452d406c408eb232a48ca7bfe
codercocoon <mounir.sahrani@gmail.com>
2017-04-11 18:44:06 +0100

    Revert "Revert "This example presents the basic concept of insert/delete a node of a linked list.""
    This reverts commit d5e9646e5327dfc62d88c391845a630e772328f1.

--- src/com/codercocoon/LinkedList/InsertDeleteANode.java
+++ src/com/codercocoon/LinkedList/InsertDeleteANode.java
@@ -1,9 +1,8 @@
-package com.codercocoon.DoublyLinkedList;
+package com.codercocoon.LinkedList;
 
 /**
- * This class represents some operations about doubly linked lists, especially
- * how you can Insert at first, at last, at Nth position, and delete from Nth
- * position.
+ * This class represents some operations about linked lists, especially how you
+ * can Insert at first, at last, at Nth position, and delete from Nth position.
  * 
  * @author contact@codercocoon.com.
  *
@@ -11,52 +10,56 @@ package com.codercocoon.DoublyLinkedList;
 public class InsertDeleteANode {
 
 	/**
-	 * This method inserts element at first of the doubly linked list.
+	 * This method inserts element at first of the linked list.
 	 * 
 	 * @param head
-	 *            : head of the doubly linked list.
+	 *            : head of the linked list.
 	 * @param _data
 	 *            : the element to insert.
 	 * @return
 	 */
 	public Node insertAtFirst(Node head, int _data) {
+		System.out.print("Input : ");
+		print(head);
+
 		Node node = new Node(_data);
 		node.next = head;
-		head.previous = node;
 
+		System.out.print("Output : ");
 		print(node);
 		return node;
 	}
 
 	/**
-	 * This method inserts element at last of the doubly linked list.
+	 * This method inserts element at last of the linked list.
 	 * 
 	 * @param head:
-	 *            head of the doubly linked list.
+	 *            head of the linked list.
 	 * @param _data:
 	 *            the element to insert.
 	 * @return
 	 */
 	public Node insertAtLast(Node head, int _data) {
+		// System.out.print("Input : ");
+		// print(head);
 		if (head == null)
 			head = new Node(_data);
 		else {
 			Node current = head;
 			while (current.next != null)
 				current = current.next;
-			Node node = new Node(_data);
-			current.next = node;
-			node.previous = current.next;
+			current.next = new Node(_data);
 		}
+		// System.out.print("Output : ");
+		// print(head);
 		return head;
 	}
 
 	/**
-	 * This method inserts element at the Nth position of the doubly linked
-	 * list.
+	 * This method inserts element at the Nth position of the linked list.
 	 * 
 	 * @param head:head
-	 *            of the doubly linked list.
+	 *            of the linked list.
 	 * @param data:the
 	 *            element to insert.
 	 * @param position:
@@ -64,38 +67,34 @@ public class InsertDeleteANode {
 	 * @return
 	 */
 	Node InsertNth(Node head, int data, int position) {
+		System.out.print("Input : ");
+		print(head);
+
 		Node current = head;
 		Node previous = null;
-
 		int i = 1;
 		while (i < position) {
 			previous = current;
 			current = current.next;
 			i++;
 		}
-
 		Node node = new Node(data);
 		node.next = current;
-		node.previous = previous;
-		current.previous = node;
-
-		if (previous != null) {
-			previous.next = node;
-		}
-
-		if (i == 1)
+		if (previous == null)
 			head = node;
+		else
+			previous.next = node;
 
+		System.out.print("Output : ");
 		print(head);
 		return head;
 	}
 
 	/**
-	 * This method deletes element at the Nth position from the doubly linked
-	 * list.
+	 * This method deletes element at the Nth position from the linked list.
 	 * 
 	 * @param head:head
-	 *            of the doubly linked list.
+	 *            of the linked list.
 	 * @param position:the
 	 *            Nth position.
 	 * @return
@@ -110,15 +109,10 @@ public class InsertDeleteANode {
 			current = current.next;
 			i++;
 		}
-
-		if (previous != null) {
+		if (previous != null)
 			previous.next = current.next;
-			if (current.next != null)
-				current.next.previous = previous;
-		} else {
+		else
 			head = current.next;
-			head.previous = null;
-		}
 
 		System.out.print("Output : ");
 		print(head);
@@ -126,10 +120,10 @@ public class InsertDeleteANode {
 	}
 
 	/**
-	 * This method prints the elements of a doubly linked list.
+	 * This method prints the elements of a linked list.
 	 * 
 	 * @param head:head
-	 *            of the doubly linked list
+	 *            of the linked list
 	 */
 	void print(Node head) {
 		if (head == null)
@@ -137,7 +131,7 @@ public class InsertDeleteANode {
 		else {
 			Node current = head;
 			while (current != null) {
-				System.out.print(current.data + (current.next != null ? "<" : "") + "->");
+				System.out.print(current.data + "->");
 				current = current.next;
 				System.out.print((current == null) ? "NULL" : "");
 			}
